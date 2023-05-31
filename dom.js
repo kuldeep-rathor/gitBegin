b7815e5e599c30f5e7f360aa3ea86be8013b46ee
kuldeep-rathor <kuldeeprathor91010@gmail.com>
2023-05-31 10:01:09 +0530

    domp1

---
+++ dom.js
@@ -0,0 +1,16 @@
+// document.all[10].textContent= 'No Of Items';
+// console.log(document.all[10]);
+var headerTitle= document.getElementById('header-title');
+var headerTitle= document.getElementById('main-header');
+
+ headerTitle.style.borderBottom ='solid 3px #000'
+
+//  console.log(document.domain);
+//  console.log(document.URL);
+//  console.log(document.title);
+//  console.log(document.doctype);
+//  console.log(document.head);
+//  console.log(document.body);
+//  console.log(document.forms);
+//  console.log(document.links);
+//  console.log(document.images);
