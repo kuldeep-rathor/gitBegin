4b0ac0ccb10ffa91125002911de764a11c8a4174
kuldeep-rathor <kuldeeprathor91010@gmail.com>
2023-05-31 10:17:39 +0530

    someChanges

--- dom.js
+++ dom.js
@@ -5,12 +5,12 @@ var headerTitle= document.getElementById('main-header');
 
  headerTitle.style.borderBottom ='solid 3px #000'
 
-//  console.log(document.domain);
-//  console.log(document.URL);
-//  console.log(document.title);
-//  console.log(document.doctype);
-//  console.log(document.head);
-//  console.log(document.body);
-//  console.log(document.forms);
-//  console.log(document.links);
-//  console.log(document.images);
+ console.log(document.domain);
+ console.log(document.URL);
+ console.log(document.title);
+ console.log(document.doctype);
+ console.log(document.head);
+ console.log(document.body);
+ console.log(document.forms);
+ console.log(document.links);
+ console.log(document.images);
